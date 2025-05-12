2e1d5aebcf067d242bc63e3cf71d0b543abad1ed
acaiexpress <acaiexpresspagamentos@gmail.com>
2025-05-12 07:27:42 +0100

    Update locations to only include Maranhão

--- src/data/locations.ts
+++ src/data/locations.ts
@@ -1,132 +1,26 @@
-export const states = [
-  "Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", 
-  "Distrito Federal", "Espírito Santo", "Goiás", "Maranhão", 
-  "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", 
-  "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", 
-  "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", 
-  "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"
-];
+export const states = ["Maranhão"];
 
 export const cities: Record<string, string[]> = {
-  "São Paulo": [
-    "São Paulo", "Guarulhos", "Campinas", "São Bernardo do Campo", "Santo André",
-    "Osasco", "São José dos Campos", "Ribeirão Preto", "Sorocaba", "Santos",
-    "Mauá", "São José do Rio Preto", "Mogi das Cruzes", "Diadema", "Jundiaí",
-    "Piracicaba", "Carapicuíba", "Bauru", "Itaquaquecetuba", "São Vicente"
-  ],
-  "Rio de Janeiro": [
-    "Rio de Janeiro", "São Gonçalo", "Duque de Caxias", "Nova Iguaçu", "Niterói",
-    "Belford Roxo", "Campos dos Goytacazes", "São João de Meriti", "Petrópolis",
-    "Volta Redonda", "Macaé", "Magé", "Itaboraí", "Cabo Frio", "Angra dos Reis"
-  ],
-  "Bahia": [
-    "Salvador", "Feira de Santana", "Vitória da Conquista", "Camaçari", "Itabuna",
-    "Juazeiro", "Lauro de Freitas", "Ilhéus", "Jequié", "Teixeira de Freitas",
-    "Barreiras", "Alagoinhas", "Porto Seguro", "Simões Filho", "Paulo Afonso"
-  ],
-  "Minas Gerais": [
-    "Belo Horizonte", "Uberlândia", "Contagem", "Juiz de Fora", "Betim",
-    "Montes Claros", "Ribeirão das Neves", "Uberaba", "Governador Valadares",
-    "Ipatinga", "Sete Lagoas", "Divinópolis", "Santa Luzia", "Ibirité", "Poços de Caldas"
-  ],
-  "Paraná": [
-    "Curitiba", "Londrina", "Maringá", "Ponta Grossa", "Cascavel",
-    "São José dos Pinhais", "Foz do Iguaçu", "Colombo", "Guarapuava",
-    "Paranaguá", "Apucarana", "Toledo", "Araucária", "Pinhais", "Campo Largo"
-  ],
-  "Rio Grande do Sul": [
-    "Porto Alegre", "Caxias do Sul", "Pelotas", "Canoas", "Santa Maria",
-    "Gravataí", "Viamão", "Novo Hamburgo", "São Leopoldo", "Rio Grande",
-    "Alvorada", "Passo Fundo", "Sapucaia do Sul", "Cachoeirinha", "Guaíba"
-  ],
-  "Pernambuco": [
-    "Recife", "Jaboatão dos Guararapes", "Olinda", "Caruaru", "Petrolina",
-    "Paulista", "Cabo de Santo Agostinho", "Camaragibe", "Garanhuns",
-    "Vitória de Santo Antão", "Igarassu", "São Lourenço da Mata", "Abreu e Lima"
-  ],
-  "Ceará": [
-    "Fortaleza", "Caucaia", "Juazeiro do Norte", "Maracanaú", "Sobral",
-    "Crato", "Itapipoca", "Maranguape", "Iguatu", "Quixadá",
-    "Pacatuba", "Aquiraz", "Russas", "Aracati", "Horizonte"
-  ],
-  "Pará": [
-    "Belém", "Ananindeua", "Santarém", "Marabá", "Castanhal",
-    "Parauapebas", "Abaetetuba", "Cametá", "Bragança", "Altamira",
-    "Marituba", "Tucuruí", "Paragominas", "Barcarena", "Tailândia"
-  ],
-  "Santa Catarina": [
-    "Joinville", "Florianópolis", "Blumenau", "São José", "Criciúma",
-    "Chapecó", "Itajaí", "Jaraguá do Sul", "Lages", "Palhoça",
-    "Balneário Camboriú", "Brusque", "Tubarão", "São Bento do Sul", "Caçador"
-  ],
-  "Goiás": [
-    "Goiânia", "Aparecida de Goiânia", "Anápolis", "Rio Verde", "Luziânia",
-    "Águas Lindas de Goiás", "Valparaíso de Goiás", "Trindade", "Formosa",
-    "Novo Gama", "Senador Canedo", "Catalão", "Itumbiara", "Jataí", "Planaltina"
-  ],
-  "Espírito Santo": [
-    "Vila Velha", "Serra", "Cariacica", "Vitória", "Cachoeiro de Itapemirim",
-    "Linhares", "São Mateus", "Guarapari", "Colatina", "Aracruz",
-    "Nova Venécia", "Barra de São Francisco", "Santa Maria de Jetibá"
-  ],
-  "Alagoas": [
-    "Maceió", "Arapiraca", "Rio Largo", "Palmeira dos Índios", "União dos Palmares",
-    "São Miguel dos Campos", "Penedo", "Marechal Deodoro", "Campo Alegre", "Delmiro Gouveia"
-  ],
-  "Piauí": [
-    "Teresina", "Parnaíba", "Picos", "Piripiri", "Campo Maior",
-    "Floriano", "Barras", "União", "Altos", "Pedro II"
-  ],
   "Maranhão": [
-    "São Luís", "Imperatriz", "São José de Ribamar", "Timon", "Caxias",
-    "Codó", "Paço do Lumiar", "Açailândia", "Bacabal", "Balsas"
-  ],
-  "Mato Grosso": [
-    "Cuiabá", "Várzea Grande", "Rondonópolis", "Sinop", "Tangará da Serra",
-    "Cáceres", "Sorriso", "Lucas do Rio Verde", "Primavera do Leste", "Barra do Garças"
-  ],
-  "Rio Grande do Norte": [
-    "Natal", "Mossoró", "Parnamirim", "São Gonçalo do Amarante", "Macaíba",
-    "Ceará-Mirim", "Caicó", "Assu", "Nova Cruz", "Currais Novos"
-  ],
-  "Paraíba": [
-    "João Pessoa", "Campina Grande", "Santa Rita", "Patos", "Bayeux",
-    "Cabedelo", "Sousa", "Cajazeiras", "Guarabira", "Sapé"
-  ],
-  "Amazonas": [
-    "Manaus", "Parintins", "Itacoatiara", "Manacapuru", "Coari",
-    "Tefé", "Tabatinga", "Maués", "Iranduba", "Humaitá"
-  ],
-  "Mato Grosso do Sul": [
-    "Campo Grande", "Dourados", "Três Lagoas", "Corumbá", "Ponta Porã",
-    "Naviraí", "Nova Andradina", "Aquidauana", "Sidrolândia", "Maracaju"
-  ],
-  "Sergipe": [
-    "Aracaju", "Nossa Senhora do Socorro", "São Cristóvão", "Lagarto", "Itabaiana",
-    "Estância", "Tobias Barreto", "Capela", "Poço Redondo", "Canindé de São Francisco"
-  ],
-  "Rondônia": [
-    "Porto Velho", "Ji-Paraná", "Ariquemes", "Vilhena", "Cacoal",
-    "Rolim de Moura", "Jaru", "Guajará-Mirim", "Pimenta Bueno", "Ouro Preto do Oeste"
-  ],
-  "Tocantins": [
-    "Palmas", "Araguaína", "Gurupi", "Porto Nacional", "Paraíso do Tocantins",
-    "Colinas do Tocantins", "Guaraí", "Tocantinópolis", "Miracema do Tocantins"
-  ],
-  "Acre": [
-    "Rio Branco", "Cruzeiro do Sul", "Sena Madureira", "Tarauacá", "Feijó",
-    "Brasileia", "Epitaciolândia", "Senador Guiomard", "Plácido de Castro"
-  ],
-  "Amapá": [
-    "Macapá", "Santana", "Laranjal do Jari", "Oiapoque", "Mazagão",
-    "Porto Grande", "Tartarugalzinho", "Pedra Branca do Amapari", "Vitória do Jari"
-  ],
-  "Roraima": [
-    "Boa Vista", "Rorainópolis", "Caracaraí", "Alto Alegre", "Mucajaí",
-    "Cantá", "Bonfim", "Pacaraima", "São João da Baliza", "São Luiz"
-  ],
-  "Distrito Federal": [
-    "Brasília", "Ceilândia", "Taguatinga", "Planaltina", "Guará",
-    "Samambaia", "Santa Maria", "Recanto das Emas", "Águas Claras", "Sobradinho"
+    "São Luís",
+    "Imperatriz",
+    "São José de Ribamar",
+    "Timon",
+    "Caxias",
+    "Codó",
+    "Paço do Lumiar",
+    "Açailândia",
+    "Bacabal",
+    "Balsas",
+    "Santa Inês",
+    "Barra do Corda",
+    "Pinheiro",
+    "Chapadinha",
+    "Santa Luzia",
+    "Buriticupu",
+    "Grajaú",
+    "Tutóia",
+    "Zé Doca",
+    "Coroatá"
   ]
 };
